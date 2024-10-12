1718e044ef575a4ad1b549932f9d9bdb723aa7bf
Tim White <tr.white@outlook.com>
2024-10-12 16:12:15 +0100

    Some tests for describe statement

--- src/interpreter/interpreter_test.go
+++ src/interpreter/interpreter_test.go
@@ -1,9 +1,13 @@
 package interpreter
 
 import (
+	"io"
+	"os"
 	"runny/src/token"
 	"runny/src/tree"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -30,4 +34,49 @@ func TestInterpreter_VisitConfigStatement(t *testing.T) {
 			t.Errorf("config value was not set")
 		}
 	})
+}
+
+func TestInterpreter_VisitDescribeStatement(t *testing.T) {
+	t.Run("description statement printed", func(t *testing.T) {
+		i := New(origin)
+		output, _ := captureOutput(func() error {
+			i.VisitDescribeStatement(tree.DescribeStatement{
+				Lines: []tree.Literal{
+					{
+						Value: "the command does X",
+					},
+				},
+			})
+			return nil
+		})
+		assert.Equal(t, "> the command does X\n", output)
+	})
+	t.Run("multiple description statements printed", func(t *testing.T) {
+		i := New(origin)
+		output, _ := captureOutput(func() error {
+			i.VisitDescribeStatement(tree.DescribeStatement{
+				Lines: []tree.Literal{
+					{
+						Value: "the command does X",
+					},
+					{
+						Value: "the command does Y",
+					},
+				},
+			})
+			return nil
+		})
+		assert.Equal(t, "> the command does X\n> the command does Y\n", output)
+	})
+}
+
+func captureOutput(f func() error) (string, error) {
+	orig := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	err := f()
+	os.Stdout = orig
+	w.Close()
+	out, _ := io.ReadAll(r)
+	return string(out), err
 }
